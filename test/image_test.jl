2d4578f5cfa2c5da1cb4306a07129d97646a0e2e
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-05-16 10:07:14 +0900

    feat: Image color test

--- test/image_test.jl
+++ test/image_test.jl
@@ -3,6 +3,16 @@ using TrussC; tc = TrussC
 
 @testset "Image Test" begin
 
+    function color_eq(a, b, d=0.1)
+        flag = (abs(tc.r(a) - tc.r(b)) < d) && (abs(tc.g(a) - tc.g(b)) < d) && (abs(tc.b(a) - tc.b(b)) < d) && (abs(tc.a(a) - tc.a(b)) < d)
+
+        if !flag
+            println(a, " !=", b)
+        end
+
+        return flag
+    end
+
     img = Image()
     @test isAllocated(img) == false
 
@@ -10,5 +20,15 @@ using TrussC; tc = TrussC
     @test isAllocated(img) == true
     @test getWidth(img) == 10
     @test getHeight(img) == 10
+
+    c1 = getColor(img, 0, 0)
+    c2 = getColor(img, 0, 9)
+    c3 = getColor(img, 9, 0)
+    c4 = getColor(img, 9, 9)
+    @test color_eq(c1, Color_red())
+    @test color_eq(c2, Color_green())
+    @test color_eq(c3, Color_blue())
+    @test color_eq(c4, Color_yellow())
+
 
 end
